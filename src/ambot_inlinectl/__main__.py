12a8166f420ff15cb2abcfb07b73c15cb884b007
RAX4096 <windowserror@163.com>
2026-06-21 12:16:07 +0800

    Add run command

--- src/ambot_inlinectl/__main__.py
+++ src/ambot_inlinectl/__main__.py
@@ -1,3 +1,6 @@
+import subprocess
+
+import amrita
 import click
 from amrita import prepare_nb_cli, prepare_orm
 
@@ -18,6 +21,37 @@ def call_nb_cli(args):
 @click.argument("args", nargs=-1, type=click.UNPROCESSED)
 def call_orm_cli(args):
     prepare_orm()(list(args), standalone_mode=False)
+
+
+@main.command("run")
+def run():
+    """Run the bot"""
+    click.echo("Starting...")
+    amrita.init()
+    amrita.load_plugins()
+    amrita.run()
+
+
+@main.command("run_in_sub")
+def run_in_sub():
+    """Run the bot in a subprocess"""
+    click.echo("Running in subprocess...")
+    subprocess.call(["ambot", "run"])
+
+
+@main.command("moo", hidden=True)
+def moo():
+    """Moo!"""
+    click.echo(r"""
+  ---------------------
+< Ambot says: Mooooo! >
+  ---------------------
+         \   ^__^
+          \  (oo)\_______
+             (__)\       )\/\
+                 ||----w |
+                 ||     ||
+""")
 
 
 if __name__ == "__main__":
